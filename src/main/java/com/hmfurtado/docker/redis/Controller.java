bbc82ee9aa3463d33309be80c0dbff30b95ce7d2
Heitor Furtado <heitormf@live.com>
2022-02-25 06:40:47 -0300

    Create dockerfile to execute app in Container with Redis in container too. Change the gateway network container if necessary (when create Jedis)

--- src/main/java/com/hmfurtado/docker/redis/Controller.java
+++ src/main/java/com/hmfurtado/docker/redis/Controller.java
@@ -12,8 +12,15 @@ public class Controller {
 
     @GetMapping
     public ResponseEntity<Long> visitorCounter() {
-        Jedis jedis = new Jedis("redis://localhost:6379");
-        jedis.set("visitors", "0");
+        Jedis jedis = new Jedis("redis://172.17.0.1:6379");
         return ResponseEntity.ok().body(jedis.incr("visitors"));
     }
+
+    @GetMapping("/reset")
+    public ResponseEntity<String> visitorCounterReset() {
+        Jedis jedis = new Jedis("redis://172.17.0.1:6379");
+        jedis.set("visitors", "0");
+        return ResponseEntity.ok().body("reseted");
+    }
+
 }
